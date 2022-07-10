4efc9d8a26a0a9d7b8b02ac6c24e0f74f662bd14
Predo <preston.dresser16@gmail.ca>
2022-07-09 21:31:56 -0400

    updated hmtl and test case

--- test_server.py
+++ test_server.py
@@ -28,7 +28,7 @@ def new_max_connection(dut, driver):
     dut.expect('response headers sent')
     dut.expect('deleted request')
     
-def new_req(dut, driver):
+def send_req(dut, driver):
     driver.get('http://192.168.2.102/predoServer')
     dut.expect('processing new request on socket')
     dut.expect('received URI = /predoServer')
@@ -39,6 +39,23 @@ def close_connection(dut, driver):
     driver.close()
     dut.expect('deleting session on fd')
     dut.expect('httpd_server: doing select')
+    
+def send_shutdown(dut, driver):
+    driver.get('http://192.168.2.102/stop')
+    dut.expect('session available, starting to accept')
+    dut.expect('accepted new fd =')
+    dut.expect('connection complete')
+    dut.expect('processing new request on socket')
+    dut.expect('received URI = /stop')
+    dut.expect('response headers sent')
+    dut.expect('deleted request')
+    dut.expect('doing select maxfd')
+    dut.expect('processing ctrl message on')
+    dut.expect('deleting session on fd')
+    dut.expect('removing /predoServer')
+    dut.expect('removing /stop')
+    dut.expect('removing /favicon.ico')
+    dut.expect('server stopped')
    
 def test_single_client(dut):
     dut.expect('main: Server Started')
@@ -49,7 +66,7 @@ def test_single_client(dut):
     driver1 = webdriver.Chrome(options=chrome_options)
     new_connection(dut, driver1)
     time.sleep(2)
-    new_req(dut, driver1)
+    send_req(dut, driver1)
     time.sleep(1)
     close_connection(dut, driver1)
     time.sleep(1)
@@ -82,4 +99,15 @@ def test_multi_client(dut):
     close_connection(dut, driver3)
     time.sleep(1)  
     close_connection(dut, driver4)
-    time.sleep(1)    
+    time.sleep(1)
+    
+def test_basic_shutdown(dut):
+    dut.expect('main: Server Started')
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    driver = webdriver.Chrome(options=chrome_options)
+    send_shutdown(dut, driver)
+    
+    
