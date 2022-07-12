f838f272707a208869320112d10169afb512a0dc
Predo <preston.dresser16@gmail.ca>
2022-07-11 20:16:01 -0400

    added env var for ESP ip address

--- test_server.py
+++ test_server.py
@@ -1,10 +1,11 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+import os
 #pytest -s shows log o/p
 
-def new_connection(dut, driver):
-    driver.get('http://192.168.2.102/predoServer')
+def new_connection(dut, driver, ip):
+    driver.get(ip + '/predoServer')
     dut.expect('session available, starting to accept')
     dut.expect('accepted new fd =')
     dut.expect('connection complete')
@@ -13,8 +14,8 @@ def new_connection(dut, driver):
     dut.expect('response headers sent')
     dut.expect('deleted request')
     
-def new_max_connection(dut, driver):
-    driver.get('http://192.168.2.102/predoServer')
+def new_max_connection(dut, driver, ip):
+    driver.get(ip + '/predoServer')
     dut.expect('no free sessions, closing least recently used')
     dut.expect('processing ctrl message')
     dut.expect('deleting session on fd')
@@ -26,8 +27,8 @@ def new_max_connection(dut, driver):
     dut.expect('response headers sent')
     dut.expect('deleted request')
     
-def send_req(dut, driver):
-    driver.get('http://192.168.2.102/predoServer')
+def send_req(dut, driver, ip):
+    driver.get(ip + '/predoServer')
     dut.expect('processing new request on socket')
     dut.expect('received URI = /predoServer')
     dut.expect('response headers sent')
@@ -38,8 +39,8 @@ def close_connection(dut, driver):
     dut.expect('deleting session on fd')
     dut.expect('httpd_server: doing select')
     
-def send_shutdown(dut, driver):
-    driver.get('http://192.168.2.102/stop')
+def send_shutdown(dut, driver, ip):
+    driver.get(ip + '/stop')
     dut.expect('session available, starting to accept')
     dut.expect('accepted new fd =')
     dut.expect('connection complete')
@@ -56,25 +57,27 @@ def send_shutdown(dut, driver):
     dut.expect('server stopped')
    
 def test_single_client(dut):
-    dut.expect('main: Server Started')
+    ip = 'http://' + str(os.environ.get('ESP_IP'))
+    dut.expect('main: Server Started', timeout=60)
     chrome_options = Options()
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
     driver1 = webdriver.Chrome(options=chrome_options)
-    new_connection(dut, driver1)
+    new_connection(dut, driver1, ip)
     time.sleep(2)
-    send_req(dut, driver1)
+    send_req(dut, driver1, ip)
     time.sleep(1)
     close_connection(dut, driver1)
     time.sleep(1)
     driver1 = webdriver.Chrome(options=chrome_options)
-    new_connection(dut, driver1)
+    new_connection(dut, driver1, ip)
     time.sleep(1)
     close_connection(dut, driver1)
     time.sleep(1)
     
 def test_multi_client(dut):
+    ip = 'http://' + str(os.environ.get('ESP_IP'))
     dut.expect('main: Server Started')
     chrome_options = Options()
     chrome_options.add_argument('--headless')
@@ -84,13 +87,13 @@ def test_multi_client(dut):
     driver2 = webdriver.Chrome(options=chrome_options)
     driver3 = webdriver.Chrome(options=chrome_options)
     driver4 = webdriver.Chrome(options=chrome_options)
-    new_connection(dut, driver1)
+    new_connection(dut, driver1, ip)
     time.sleep(3)
-    new_connection(dut, driver2)
+    new_connection(dut, driver2, ip)
     time.sleep(3)
-    new_connection(dut, driver3)
+    new_connection(dut, driver3, ip)
     time.sleep(3)
-    new_max_connection(dut, driver4)
+    new_max_connection(dut, driver4, ip)
     time.sleep(5)
     close_connection(dut, driver2)
     time.sleep(1)  
@@ -100,12 +103,13 @@ def test_multi_client(dut):
     time.sleep(1)
 
 def test_basic_shutdown(dut):
+    ip = 'http://' + str(os.environ.get('ESP_IP'))
     dut.expect('main: Server Started')
     chrome_options = Options()
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
     driver = webdriver.Chrome(options=chrome_options)
-    send_shutdown(dut, driver)
+    send_shutdown(dut, driver, ip)
     
    
